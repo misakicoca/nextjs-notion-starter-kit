ceaa70aad75ef5ce07fb0871468fbac682b285cb
Coca <misakicoca@gmail.com>
2022-07-02 21:00:52 +0800

    Update ICON and Copyright

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -35,7 +35,7 @@ export const FooterImpl: React.FC = () => {
 
   return (
     <footer className={styles.footer}>
-      <div className={styles.copyright}>Copyright 2021 {config.author} | Font Designed by Phil Pluckthun</div>
+      <div className={styles.copyright}>Copyright 2022 {config.author} | Font Designed by Phil Pluckthun</div>
 
       <div className={styles.settings}>
         {hasMounted && (
